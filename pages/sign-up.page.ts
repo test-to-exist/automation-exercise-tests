d397d37d2e2df8fa41d641795a10e2e9e43359ae
Tomasz Pichlak <testtoexist@fastmail.com>
2025-06-25 14:20:54 +0200

    code cleanings

--- pages/sign-up.page.ts
+++ pages/sign-up.page.ts
@@ -1,4 +1,4 @@
-import { expect, Locator, Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { AccountInformationPage } from "@pages/account-information.page";
 
 export class SignupPage {
