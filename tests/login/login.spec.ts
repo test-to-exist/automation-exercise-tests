5ff0a82caaf29c15a7b6e878a2c6a7eceb33d53a
Tomasz Pichlak <testtoexist@fastmail.com>
2025-10-12 16:23:41 +0200

    Add login negative test

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -2,22 +2,40 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "@pages/login.page";
 import { AcceptCookiesPage } from "@pages/accept-cookies.page";
 import { isNil } from "lodash";
+import { faker } from "@faker-js/faker";
 
-test("User logs in successfully", async ({ page }, { parallelIndex }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
+test.describe("Login Tests", () => {
+  let loginPage: LoginPage;
+  let env: object;
 
-  await expect(loginPage.loginHeader).toBeVisible();
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await expect(loginPage.loginHeader).toBeVisible();
 
-  const env = process.env;
-  if (isNil(env["CI"]) || env["CI"] === "false") {
-    const acceptCookiesPage = new AcceptCookiesPage(page);
-    await acceptCookiesPage.consentButton.click();
-  }
-  const username = env[`USERNAME${parallelIndex}`];
-  const password = env[`PASSWORD${parallelIndex}`];
+    env = process.env;
+    if (isNil(env["CI"]) || env["CI"] === "false") {
+      const acceptCookiesPage = new AcceptCookiesPage(page);
+      await acceptCookiesPage.consentButton.click();
+    }
+  });
 
-  await loginPage.login(username, password);
+  test("User logs in successfully", async ({ page }, { parallelIndex }) => {
+    const username = env[`USERNAME${parallelIndex}`];
+    const password = env[`PASSWORD${parallelIndex}`];
 
-  await expect(page.getByText("Logged in as")).toBeVisible();
+    await loginPage.login(username, password);
+
+    await expect(page.getByText("Logged in as")).toBeVisible();
+  });
+
+  test("User fails to log in with incorrect password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(faker.internet.email(), "incorrectPassword123");
+
+    await expect(
+      page.getByText("Your email or password is incorrect!")
+    ).toBeVisible();
+  });
 });
