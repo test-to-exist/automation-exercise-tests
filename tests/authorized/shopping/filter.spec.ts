5fa7131619e5b53a9905a08bbc2b670f36c58ec4
Tomasz Pichlak <testtoexist@fastmail.com>
2025-11-18 16:02:23 +0100

    Add filters test

--- tests/authorized/shopping/filter.spec.ts
+++ tests/authorized/shopping/filter.spec.ts
@@ -1,9 +1,17 @@
-import { test } from "@fixtures/auth-fixture";
+import { expect, test } from "@fixtures/auth-fixture";
 
 test.describe("Shopping Filters Tests", () => {
   test.describe("Womens filters", () => {
     test("User should be able to filter by dress", async ({ page }) => {
+      await page.goto(`${process.env.BASE_URL}`);
+      await page.getByRole("link", { name: "Women" }).click();
       await page.getByRole("link", { name: "Dress" }).click();
+      const productNames = await page.locator(".product-overlay * p").count();
+      for (let i = 0; i < productNames; i++) {
+        await expect(page.locator(".product-overlay * p").nth(i)).toContainText(
+          "Dress"
+        );
+      }
     });
   });
 
