cbdf6c72c918c3facbcc2985a14f40b935ea43b7
Tomasz Pichlak <testtoexist@fastmail.com>
2025-11-28 19:37:44 +0100

    Move locators to class

--- pages/products.page.ts
+++ pages/products.page.ts
@@ -1,11 +1,46 @@
 import { Locator, Page } from "@playwright/test";
 import { ProductAddedModal } from "@pages/product-added.modal";
 
+type WomensCategories = {
+  dress: Locator;
+  tops: Locator;
+  saree: Locator;
+};
+
+type MensCategories = {
+  tshirts: Locator;
+  jeans: Locator;
+};
+
+type KidsCategories = {
+  dress: Locator;
+  topsAndShirts: Locator;
+};
+
 export class ProductsPage {
   readonly page: Page;
+  readonly categoryMens: Locator;
+  readonly categoryWomens: Locator;
+  readonly categoryKids: Locator;
+  readonly mensCategories: MensCategories;
+  readonly womensCategories: WomensCategories;
+  readonly kidsCategories: KidsCategories;
+  readonly productName: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.categoryMens = page.getByRole("link", { name: " Men" });
+    this.categoryWomens = page.getByRole("link", { name: "Women" });
+    this.categoryKids = page.getByRole("link", { name: "Kids" }).first();
+    this.womensCategories = {} as WomensCategories;
+    this.womensCategories.dress = page.getByRole("link", { name: "Dress" });
+    this.mensCategories = {} as MensCategories;
+    this.mensCategories.tshirts = page.getByRole("link", { name: "Tshirts" });
+    this.kidsCategories = {} as KidsCategories;
+    this.kidsCategories.topsAndShirts = page.getByRole("link", {
+      name: "Tops & Shirts",
+    });
+    this.productName = page.locator(".product-overlay * p");
   }
 
   async addItemById(id: number) {
--- tests/authorized/shopping/filter.spec.ts
+++ tests/authorized/shopping/filter.spec.ts
@@ -1,15 +1,30 @@
 import { expect, test } from "@fixtures/auth-fixture";
+import { ProductsPage } from "@pages/products.page";
 
 test.describe("Shopping Filters Tests", () => {
   test.describe("Womens filters", () => {
     test("User should be able to filter by Womens dress", async ({ page }) => {
       await page.goto(`${process.env.BASE_URL}`);
-      await page.getByRole("link", { name: "Women" }).click();
-      await page.getByRole("link", { name: "Dress" }).click();
-      const productNames = await page.locator(".product-overlay * p").count();
+      const productsPage = new ProductsPage(page);
+      await productsPage.categoryWomens.click();
+      await productsPage.womensCategories.dress.click();
+      const productNames = await productsPage.productName.count();
       for (let i = 0; i < productNames; i++) {
-        await expect(page.locator(".product-overlay * p").nth(i)).toContainText(
-          "Dress"
+        await expect(productsPage.productName.nth(i)).toContainText("Dress");
+      }
+    });
+  });
+
+  test.describe("Mens filters", () => {
+    test("User should be able to filter by Mens t-shirts", async ({ page }) => {
+      await page.goto(`${process.env.BASE_URL}`);
+      const productsPage = new ProductsPage(page);
+      await productsPage.categoryMens.click();
+      await productsPage.mensCategories.tshirts.click();
+      const productNames = await productsPage.productName.count();
+      for (let i = 0; i < productNames; i++) {
+        await expect(productsPage.productName.nth(i)).toContainText(
+          /(Tshirt|T-Shirt|T\sSHIRT)/
         );
       }
     });
@@ -20,28 +35,13 @@ test.describe("Shopping Filters Tests", () => {
       page,
     }) => {
       await page.goto(`${process.env.BASE_URL}`);
-      await page.getByRole("link", { name: "Kids" }).first().click();
-      await page.getByRole("link", { name: "Tops & Shirts" }).click();
-      const productNames = await page.locator(".product-overlay * p").count();
+      const productsPage = new ProductsPage(page);
+      await productsPage.categoryKids.click();
+      await productsPage.kidsCategories.topsAndShirts.click();
+      const productNames = await productsPage.productName.count();
       for (let i = 0; i < productNames; i++) {
-        await expect(page.locator(".product-overlay * p").nth(i)).toContainText(
+        await expect(productsPage.productName.nth(i)).toContainText(
           /(Top|Shirt)/i
-        );
-      }
-    });
-  });
-
-  test.describe("Mens filters", () => {
-    test("User should be able to filter by Mens t-shirts", async ({ page }) => {
-      await page.goto(`${process.env.BASE_URL}`);
-      await page.getByRole("link", { name: " Men" }).click();
-
-      await page.locator("//a[contains(text(),'Tshirts')]").hover();
-      await page.locator("//a[contains(text(),'Tshirts')]").click();
-      const productNames = await page.locator(".product-overlay * p").count();
-      for (let i = 0; i < productNames; i++) {
-        await expect(page.locator(".product-overlay * p").nth(i)).toContainText(
-          /(Tshirt|T-Shirt|T\sSHIRT)/
         );
       }
     });
