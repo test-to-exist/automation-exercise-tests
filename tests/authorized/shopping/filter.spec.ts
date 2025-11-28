d545a2f419e9d439b94015032c0e23437e54f35c
Tomasz Pichlak <testtoexist@fastmail.com>
2025-11-28 13:38:36 +0100

    Add test for kids filters

--- tests/authorized/shopping/filter.spec.ts
+++ tests/authorized/shopping/filter.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@fixtures/auth-fixture";
 
 test.describe("Shopping Filters Tests", () => {
   test.describe("Womens filters", () => {
-    test("User should be able to filter by dress", async ({ page }) => {
+    test("User should be able to filter by Womens dress", async ({ page }) => {
       await page.goto(`${process.env.BASE_URL}`);
       await page.getByRole("link", { name: "Women" }).click();
       await page.getByRole("link", { name: "Dress" }).click();
@@ -15,7 +15,21 @@ test.describe("Shopping Filters Tests", () => {
     });
   });
 
-  test.describe("Mens filters", () => {});
+  test.describe("Kids filters", () => {
+    test("User should be able to filter by kids tops & shirts", async ({
+      page,
+    }) => {
+      await page.goto(`${process.env.BASE_URL}`);
+      await page.getByRole("link", { name: "Kids" }).first().click();
+      await page.getByRole("link", { name: "Tops & Shirts" }).click();
+      const productNames = await page.locator(".product-overlay * p").count();
+      for (let i = 0; i < productNames; i++) {
+        await expect(page.locator(".product-overlay * p").nth(i)).toContainText(
+          /(Top|Shirt)/i
+        );
+      }
+    });
+  });
 
-  test.describe("Kids filters", () => {});
+  test.describe("Mens filters", () => {});
 });
